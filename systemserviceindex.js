88386b92611fae8da3fe687ef28c358310c567bf
SavithaKaramchedu <47734605+SavithaKaramchedu@users.noreply.github.com>
2019-07-29 14:44:30 +0530

    Delete systemserviceindex.js

--- systemserviceindex.js
+++
@@ -1,8 +0,0 @@
-const app = require('purplecheerio-wave');
-const initoptions={
- "aclconfig":"aclconfig.json",
- "secret":"helloworld",
- "appserviceconfig":"appserviceconfig.json",
- "appdir":__dirname
-} 
-app.startsystemservice("keymgr",initoptions);
